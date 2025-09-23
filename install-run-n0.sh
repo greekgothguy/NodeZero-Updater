b14d7cc0579f3319dda376edcaf608ef8c515358
greekgothguy <46501122+greekgothguy@users.noreply.github.com>
2025-09-23 10:35:54 +0200

    Update install-run-n0.sh

--- install-run-n0.sh
+++ install-run-n0.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-n0 -n 5
+
+LOGFILE="/var/log/run-n0.log"
+
+# Ensure log directory exists
+mkdir -p "$(dirname "$LOGFILE")"
+
+# Run n0 and append both stdout and stderr to the log
+n0 -n 5 >>"$LOGFILE" 2>&1
