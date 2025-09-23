4910bfa65a50916f75f027fff2598218b58b09a1
greekgothguy <46501122+greekgothguy@users.noreply.github.com>
2025-09-23 11:49:18 +0200

    Update install-run-n0.sh

--- install-run-n0.sh
+++ install-run-n0.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-
 LOGFILE="/var/log/run-n0.log"
-
-# Ensure log directory exists
 mkdir -p "$(dirname "$LOGFILE")"
 
-# Run n0 and append both stdout and stderr to the log
-n0 -n 5 >>"$LOGFILE" 2>&1
+# Run n0 as non-root; log as root
+sudo -u nodezero bash -c 'TERM=dumb PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" n0 -n 5' \
+  | tee -a "$LOGFILE" >/dev/null
